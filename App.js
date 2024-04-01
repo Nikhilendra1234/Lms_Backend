4eb0a6f20a24e49579e9389c2cc17cbf4f182a52
Nikhilendra1234 <nikhilendranayak@gmail.com>
2024-04-01 21:15:48 +0530

    added usefull package into app and defined to test routes

--- App.js
+++ App.js
@@ -1,5 +1,23 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
 
+dotenv.config();
 const app=express();
+app.use(express.json());
+app.use(cors({
+    origin:process.env.FRONTEND-URL,
+    Credential:true
+}));
+app.use(cookieParser());
+
+app.use("/ping",(req,res)=>{
+    res.send("hello ping");    
+})
+
+app.all('*',(req,res)=>{
+    res.send("404 , Page not Found...")
+})
 
 export default app
